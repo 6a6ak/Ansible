aef81be2289a2df1e54f678fe6a3dcf2f7acbaa3
Babak Ahari <babak.ahari@gmail.com>
2023-09-24 23:55:03 +0200

    Update genesis.sh

--- Bootstarp/genesis.sh
+++ Bootstarp/genesis.sh
@@ -2,23 +2,18 @@
 # Get the directory from the user
 echo "Enter the directory where you want to create the Ansible project:(/home/babak)"
 read directory
-
 # Create the Ansible project directory
 project_directory="${directory}/Ansible"
 mkdir -p ${project_directory}
-
 # Create the inventory file
 echo "[myhosts]" > ${project_directory}/inventory.ini
 echo "host1 ansible_host=192.168.1.2 ansible_user=username ansible_ssh_private_key_file=/path/to/private/key" >> ${project_directory}/inventory.ini
-
 # Create the roles directory
 mkdir -p ${project_directory}/roles/example_role/tasks
-
 # Create a main.yml file in the tasks directory of the example_role
 echo "---" > ${project_directory}/roles/example_role/tasks/main.yml
 echo "- name: Example task" >> ${project_directory}/roles/example_role/tasks/main.yml
 echo "  command: echo 'Hello, world!'" >> ${project_directory}/roles/example_role/tasks/main.yml
-
 # Create a playbook that uses the example_role
 echo "---" > ${project_directory}/playbook.yml
 echo "- hosts: myhosts" >> ${project_directory}/playbook.yml
